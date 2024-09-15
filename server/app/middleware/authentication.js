a8423007a4aedc83c65f77dc8d25c7fbdc0482d5
Suriya2596 <suriyajagan25@gmail.com>
2024-09-15 21:50:29 +0530

    populate user

--- server/app/middleware/authentication.js
+++ server/app/middleware/authentication.js
@@ -2,35 +2,40 @@ const jwt = require("jsonwebtoken")
 const User = require("../models/UserModel")
 
 
-const authentication = (req, res, next) => {
-    const token = req.header("Authorization").split(" ")[1]
-    try {
-        const tokenData = jwt.verify(token, process.env.JWT_Verify)
-        if (tokenData) {
-            User.findOne({ _id: tokenData._id }).populate("role")
-                .then((user) => {
-                    if (user) {
-                        req.user = user
-                        next()
-                    } else {
-                        res.status(401).json({
-                            message: "User is Not found",
-                        })
-                    }
-                })
-        } else {
-            res.status(401).json({
-                message: "Invalidate Token"
-            })
-        }
+const authentication = async (req, res, next) => {
+    const token = req.header("Authorization")?.split(" ")[1];
 
+    try {
+        const tokenData = jwt.verify(token, process.env.JWT_Verify);
+
+        if (tokenData) {
+            let user = await User.findOne({ _id: tokenData._id }).populate("role");
+
+            if (user) {
+                if (user.createdBy) {
+                    user = await User.populate(user, { path: "createdBy", select: "_id name email" });
+                }
+
+                req.user = user;
+                next();
+            } else {
+                return res.status(401).json({
+                    message: "User is not found",
+                });
+            }
+        } else {
+            return res.status(401).json({
+                message: "Invalid Token",
+            });
+        }
     } catch (error) {
-        res.status(401).json({
-            message: "Invalidate Token",
-            error: error
-        })
+        return res.status(401).json({
+            message: "Invalid Token",
+            error: error.message,
+        });
     }
-}
+};
+
 
 
 module.exports = {
